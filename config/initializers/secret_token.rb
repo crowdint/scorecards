3f87ec48cd35e6127a31fb9c1f1dd565e55532f4
Hector Bustillos & Carlos Muniz <hector.bustillos@crowdint.com>
2010-12-22 16:29:07 -0600

    rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Scorecards::Application.config.secret_token = '439ee4e270be13b9baf813beb0c315b2a39d76c98f12c3a8779002b541d802a2ef71636d02cad7df1a48f7bea93e9b08ac185200eb733afa1a03510959405245'
